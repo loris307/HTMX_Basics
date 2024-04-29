58295b8daa9b41d77ec439c4c921a817fa36db02
Loris Galler <gallerloris@gmail.com>
2024-04-29 14:48:30 +0200

    comments deleted

--- Server/server.js
+++ Server/server.js
@@ -1,41 +1,48 @@
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 8080;
 
-app.use(cors()); // Dies ermöglicht CORS für alle Routen
+
+app.use(cors());
 app.use(express.json());
 
 
-// Dummy-Daten für die Demonstration
+
+
 const messages = [
     { id: 1, text: "Diese Nachricht kommt vom Server" },
     { id: 2, text: "Diese Hier auch!" },
     { id: 3, text: "DIE NICHT (nein spaß, die auch)" }
 ];
 
+
+
 app.get('/get-messages', (req, res) => {
     const html = messages.map(m => `<div>${m.text}</div>`).join('');
     res.send(html);
 });
 
+
+
 app.post('/add-messages', (req, res) => {
     console.log(req.body);
     console.log(req.body.message);
-    const textToAdd = req.body.message; // Stellt sicher, dass du den Schlüssel 'message' erwartest
+    const textToAdd = req.body.message; 
     if(textToAdd) {
-        messages.push({ id: messages.length + 1, text: textToAdd }); // Fügt eine ID hinzu
+        messages.push({ id: messages.length + 1, text: textToAdd }); 
         const html = messages.map(m => `<div>${m.text}</div>`).join('');
-        res.send(html); // Sendet die aktualisierte Liste als HTML zurück
+        res.send(html); 
     } else {
         res.status(400).send('Keine Nachricht angegeben');
     }
 });
 
-// Route zum Löschen der letzten Nachricht
+
 app.delete('/delete-last-message', (req, res) => {
     if (messages.length > 0) {
-        messages.pop();  // Entfernt das letzte Element aus dem Array
+        messages.pop(); 
         const html = messages.map(m => `<div>${m.text}</div>`).join('');
         res.send(html);
     } else {
